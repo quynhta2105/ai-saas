82c021416bb4e5f2c7a71fa58fbcde373f6ac888
Quynh <taq2105@gmail.com>
2024-10-20 21:28:14 +0700

    init and add auth

---
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -0,0 +1,14 @@
+import { UserButton } from '@clerk/nextjs'
+import React from 'react'
+
+const DashboardPage
+ = () => {
+  return (
+    <div>
+      DashboardPage
+      <UserButton />
+    </div>
+  )
+}
+
+export default DashboardPage
---
+++ app/layout.tsx
@@ -0,0 +1,45 @@
+import type { Metadata } from "next";
+import localFont from "next/font/local";
+import "./globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
+
+const geistSans = localFont({
+  src: "./fonts/GeistVF.woff",
+  variable: "--font-geist-sans",
+  weight: "100 900",
+});
+const geistMono = localFont({
+  src: "./fonts/GeistMonoVF.woff",
+  variable: "--font-geist-mono",
+  weight: "100 900",
+});
+
+export const metadata: Metadata = {
+  title: "AI BOT",
+  description: "AI Platform",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <ClerkProvider
+      appearance={{
+        layout: {
+          unsafe_disableDevelopmentModeWarnings: true,
+        },
+      }}
+      afterSignOutUrl='/'
+    >
+      <html lang="en">
+        <body
+          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        >
+          {children}
+        </body>
+      </html>
+    </ClerkProvider>
+  );
+}
